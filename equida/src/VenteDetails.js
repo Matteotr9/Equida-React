6dd9afc2aaba41ed7961804c41f4166b45c2f0ef
Matteotr9 <89576532+Matteotr9@users.noreply.github.com>
2023-06-01 16:21:56 +0200

    Update VenteDetails.js

--- equida/src/VenteDetails.js
+++ equida/src/VenteDetails.js
@@ -1,46 +1,47 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
-const VenteDetails = ({ venteId }) => {
-  const [lots, setLots] = useState([]);
+import moment from 'moment';
+const VenteDetails = ({ vente }) => {
+  const [chevaux, setChevaux] = useState([]);
 
   useEffect(() => {
-    axios.get(`http://127.0.0.1/Equida-Spa2/public/api/vente/consulter/${venteId}`)
-      .then(response => {
-        setLots(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, [venteId]);
+    fetch(`http://127.0.0.1/Equida-Spa2/public/api/cheval/consulter/${vente.id}`)
+      .then(response => response.json())
+      .then(data => setChevaux(data.lots))
+      .catch(error => console.log(error));
+  }, [vente.id]);
+  const formatDate = date => {
+    console.log(date);
+    const formattedDate = moment(date.date, 'YYYY-MM-DD').format('DD/MM/YYYY');
+    return formattedDate;}
 
   return (
     <div>
-      <h2>Détails de la vente</h2>
-      {lots.length === 0 ? (
-        <p>Aucun lot disponible</p>
-      ) : (
-        <table>
-          <thead>
-            <tr>
-              <th>Nom du cheval</th>
-              <th>Race du cheval</th>
-              <th>Mise à prix</th>
+      <h1>
+        Vente : {vente.nom} du {formatDate(vente.dateDebut)} au {formatDate(vente.dateFin)}
+      </h1>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Nom du Cheval</th>
+            <th>Race</th>
+            <th>Mise A prix</th>
+          </tr>
+        </thead>
+        <tbody>
+          {chevaux.map(lot => (
+            <tr key={lot.id}>
+              <td>{lot.cheval.nom}</td>
+              <td>{lot.cheval.race.libellle}</td>
+              <td>{lot.miseAPrix}</td>
             </tr>
-          </thead>
-          <tbody>
-            {lots.map((lot, index) => (
-              <tr key={index}>
-                <td>{lot.lotChevalNom}</td>
-                <td>{lot.lotChevalRaceLibelle}</td>
-                <td>{lot.lotMiseAPrix}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
+
+
 
 export default VenteDetails;
