ed08cd3c55a66a75415a793658518ad61f9518f9
Matteotr9 <89576532+Matteotr9@users.noreply.github.com>
2023-06-01 16:05:57 +0200

    update

--- equida/src/VenteDetails.js
+++ equida/src/VenteDetails.js
@@ -1,47 +1,46 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const VenteDetails = ({ vente }) => {
-  const [chevaux, setChevaux] = useState([]);
+const VenteDetails = ({ venteId }) => {
+  const [lots, setLots] = useState([]);
 
   useEffect(() => {
-    fetch(`http://127.0.0.1/Equida-Spa2/public/api/cheval/consulter/${vente.id}`)
-      .then(response => response.json())
-      .then(data => setChevaux(data.lots))
-      .catch(error => console.log(error));
-  }, [vente.id]);
+    axios.get(`http://127.0.0.1/Equida-Spa2/public/api/vente/consulter/${venteId}`)
+      .then(response => {
+        setLots(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, [venteId]);
 
   return (
     <div>
-      <h1>
-        Vente : {vente.nom} du {formatDate(vente.dateDebut)} au {formatDate(vente.dateFin)}
-      </h1>
-
-      <table>
-        <thead>
-          <tr>
-            <th>Nom du Cheval</th>
-            <th>Race</th>
-            <th>Mise A prix</th>
-          </tr>
-        </thead>
-        <tbody>
-          {chevaux.map(lot => (
-            <tr key={lot.id}>
-              <td>{lot.cheval.nom}</td>
-              <td>{lot.cheval.race.libellle}</td>
-              <td>{lot.miseAPrix}</td>
+      <h2>Détails de la vente</h2>
+      {lots.length === 0 ? (
+        <p>Aucun lot disponible</p>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>Nom du cheval</th>
+              <th>Race du cheval</th>
+              <th>Mise à prix</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {lots.map((lot, index) => (
+              <tr key={index}>
+                <td>{lot.lotChevalNom}</td>
+                <td>{lot.lotChevalRaceLibelle}</td>
+                <td>{lot.lotMiseAPrix}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
-};
-
-const formatDate = date => {
-  const options = { day: 'numeric', month: 'numeric', year: 'numeric' };
-  const formattedDate = new Date(date).toLocaleDateString(undefined, options);
-  return formattedDate;
 };
 
 export default VenteDetails;
